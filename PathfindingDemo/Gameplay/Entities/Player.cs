d48e115ed087f2fb5de941bbf0dbb3f9cd1ea525
Quinn Zieltjens <99677023+QuinnZieltjens@users.noreply.github.com>
2023-06-16 11:33:26 +0200

    feat:delay_input
    added a delay to the input

--- PathfindingDemo/Gameplay/Entities/Player.cs
+++ PathfindingDemo/Gameplay/Entities/Player.cs
@@ -4,23 +4,23 @@ using PathfindingDemo.Gameplay.Util;
 namespace PathfindingDemo.Gameplay.Entities;
 internal class Player : Entity
 {
-    public Player(World _world) : base(_world, new Position(1, 0))
+    public Player(World _world) : base(_world, new Position(4, 4))
     {
         Color = ConsoleColor.Green;
     }
 
     protected override void Update()
     {
-        if (Input.KeyPressed(ConsoleKey.W))
+        if (Input.GetKeyDown(ConsoleKey.W))
             Move(Position.Up);
 
-        if (Input.KeyPressed(ConsoleKey.A))
+        if (Input.GetKeyDown(ConsoleKey.A))
             Move(Position.Left);
 
-        if (Input.KeyPressed(ConsoleKey.S))
+        if (Input.GetKeyDown(ConsoleKey.S))
             Move(Position.Down);
 
-        if (Input.KeyPressed(ConsoleKey.D))
+        if (Input.GetKeyDown(ConsoleKey.D))
             Move(Position.Right);
     }
 }
--- PathfindingDemo/Gameplay/Util/Input.cs
+++ PathfindingDemo/Gameplay/Util/Input.cs
@@ -4,14 +4,39 @@ namespace PathfindingDemo.Gameplay.Util;
 
 internal static class Input
 {
+    private const int InputDelayMilliseconds = 500;
+    private static readonly Dictionary<ConsoleKey, DateTime> keypresses = new();
+
+    public static bool GetKeyDown(ConsoleKey _key)
+    {
+        short keyState = GetAsyncKeyState((int)_key);
+        bool keyPressed = (keyState & 0x8000) != 0;
+
+        if (keyPressed == false)
+            return false;
+
+        if (keypresses.ContainsKey(_key))
+        {
+            bool timeout;
+            TimeSpan difference = (keypresses[_key] - DateTime.Now);
+            TimeSpan delay = new(0, 0, 0, 0, InputDelayMilliseconds);
+
+            timeout = difference >= delay;
+            if (timeout == false)
+            {
+                keypresses[_key] = DateTime.Now;
+                return true;
+            }
+
+            return false;
+        }
+
+        keypresses.Add(_key, DateTime.Now);
+        return true;
+    }
+
     // Import the GetAsyncKeyState function from user32.dll
     [DllImport("user32.dll")]
-    public static extern short GetAsyncKeyState(int _vKey);
+    private static extern short GetAsyncKeyState(int _vKey);
 
-    private static ConsoleKey? lastDetectedKey = null;
-
-    public static bool KeyPressed(ConsoleKey _key)
-    {
-        return (GetAsyncKeyState((int)_key) & 0x8000) != 0;
-    }
 }
