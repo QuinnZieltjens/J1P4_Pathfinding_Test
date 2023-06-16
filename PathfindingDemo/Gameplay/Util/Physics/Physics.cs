0fc9915c42bd278f463ac9b4090b636559e2535b
Quinn Zieltjens <99677023+QuinnZieltjens@users.noreply.github.com>
2023-06-16 11:32:50 +0200

    feat:added_raycasts
    I added raycasts to the game

---
+++ PathfindingDemo/Gameplay/Util/Physics/Physics.cs
@@ -0,0 +1,19 @@
+﻿using PathfindingDemo.Gameplay.Enviroment;
+
+namespace PathfindingDemo.Gameplay.Util.Physics;
+
+internal static partial class Physics
+{
+    /// <param name="_world">the world in which the raycast is ran</param>
+    /// <param name="_startPos">the starting position of the raycast</param>
+    /// <param name="_endPos">the position that the raycast will travel to</param>
+    /// <returns>an instance of <see cref="RaycastHit"/></returns>
+    public static RaycastHit DrawRay(World _world, Position _startPos, Position _endPos)
+    {
+        //create a raycast object
+        Raycast raycast = new(_world, _startPos, _endPos);
+
+        //draw the ray and return the results
+        return raycast.DrawRay();
+    }
+}
---
+++ PathfindingDemo/Gameplay/Util/RaycastHit.cs
@@ -0,0 +1,24 @@
+﻿namespace PathfindingDemo.Gameplay.Util;
+
+/// <summary>
+/// holds whether a raycast hit a wall and the position it got to (before hitting a wall if it did)
+/// </summary>
+internal struct RaycastHit
+{
+    private Position? hitPosition;
+
+    public Position HitPosition { get => hitPosition ?? throw new NullReferenceException("no position was set"); }
+    public bool Hit { get; }
+
+    public RaycastHit(bool _hit, Position _lastPos)
+    {
+        hitPosition = _lastPos;
+        Hit = _hit;
+    }
+
+    public RaycastHit(bool _hit)
+    {
+        Hit = _hit;
+        hitPosition = null;
+    }
+}
