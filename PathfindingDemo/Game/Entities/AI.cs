f8b073e97dfc52739c451b98ffbf44debd81a667
Quinn Zieltjens <99677023+QuinnZieltjens@users.noreply.github.com>
2023-06-22 10:58:07 +0200

    fix:AI_didnt_call_start
    the entities are being created in the start method so start will already have been called when they exist

--- PathfindingDemo/Game/Entities/AI.cs
+++ PathfindingDemo/Game/Entities/AI.cs
@@ -16,22 +16,26 @@ internal class AI : Entity
 
         //set the colour
         Color = ConsoleColor.Magenta;
+
+        CalculateRoute(pathfinding.Target);
     }
 
     /// <summary>
     /// calculates the shortest path to <paramref name="_target"/> and goes there
     /// </summary>
-    public async void CalculateRoute(Position _target)
+    public void CalculateRoute(Position _target)
     {
-        //get the shortest path via the pathfinding algorithm
-        pathfinding.Start = Position; //set the start node
-        pathfinding.Target = _target; //set the target node
-        await pathfinding.Run(); //run the algorithm
-    }
+        Thread pathfinderThread = new(() =>
+        {
+            //get the shortest path via the pathfinding algorithm
+            pathfinding.Start = Position; //set the start node
+            pathfinding.Target = _target; //set the target node
+            pathfinding.Run().Wait(); //run the algorithm
 
-    protected override void Start()
-    {
-        CalculateRoute(pathfinding.Target);
+            walkPath = pathfinding.GetPathPositions().ToList();
+        });
+
+        pathfinderThread.Start();
     }
 
     protected override void Update()
@@ -39,10 +43,10 @@ internal class AI : Entity
         if (walkPath.Count == 0) //if there are no positions to move
             return; //exit method
 
-        Position moveDirection = walkPath[0]; //get the next relative position to move to
+        Position movePos = walkPath[0]; //get the next relative position to move to
         walkPath.RemoveAt(0); //remove the index 0
 
         //move to the position
-        Move(moveDirection);
+        Move((Position - movePos) * -1);
     }
 }
