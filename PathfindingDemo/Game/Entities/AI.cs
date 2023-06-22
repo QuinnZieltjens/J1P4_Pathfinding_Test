03a0f008a7d736576ad83cb785d2ef87afd8cc76
Quinn Zieltjens <99677023+QuinnZieltjens@users.noreply.github.com>
2023-06-22 11:07:05 +0200

    feat:add_random_movement
    make the ai pick a new target position randomly

--- PathfindingDemo/Game/Entities/AI.cs
+++ PathfindingDemo/Game/Entities/AI.cs
@@ -17,19 +17,17 @@ internal class AI : Entity
         //set the colour
         Color = ConsoleColor.Magenta;
 
-        CalculateRoute(pathfinding.Target);
+        CalculateRoute();
     }
 
     /// <summary>
     /// calculates the shortest path to <paramref name="_target"/> and goes there
     /// </summary>
-    public void CalculateRoute(Position _target)
+    public void CalculateRoute()
     {
         Thread pathfinderThread = new(() =>
         {
             //get the shortest path via the pathfinding algorithm
-            pathfinding.Start = Position; //set the start node
-            pathfinding.Target = _target; //set the target node
             pathfinding.Run().Wait(); //run the algorithm
 
             walkPath = pathfinding.GetPathPositions().ToList();
@@ -41,12 +39,33 @@ internal class AI : Entity
     protected override void Update()
     {
         if (walkPath.Count == 0) //if there are no positions to move
+        {
+            NewRoute(); //get a new route to walk
             return; //exit method
+        }
 
         Position movePos = walkPath[0]; //get the next relative position to move to
         walkPath.RemoveAt(0); //remove the index 0
 
         //move to the position
         Move((Position - movePos) * -1);
+    }
+
+    private void NewRoute()
+    {
+        Random random = new();
+        int x;
+        int y;
+
+        do
+        {
+            x = random.Next(0, World.SizeX - 1);
+            y = random.Next(0, World.SizeY - 1);
+        }
+        while (World.IsWall(new Position(x, y)));
+
+        pathfinding.Target = new Position(x, y);
+        pathfinding.Start = Position;
+        CalculateRoute();
     }
 }
